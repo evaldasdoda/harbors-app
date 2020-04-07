b8f8fa83f547c84635455061a9a877a8fa621e6f
evaldasdoda <55666891+evaldasdoda@users.noreply.github.com>
2020-04-07 23:37:49 +0300

    Commit
    + Updated modal component (click outside)

--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -6,17 +6,43 @@ import Button from '../Button/Button';
 // Styles
 require('./Modal.scss');
 
-const modal = props => {
-    return (
-        <div className="MODAL">
-            <div className="MODAL__container">
-                <h2>{props.header}</h2>
-                <hr/>
-                <p>{props.content}</p>
-                <Button class='btn-danger' clicked={props.onButtonClick}>Close</Button>
-            </div>
-        </div>
-    );
-};
+export default class modal extends React.Component {
+    constructor(props) {
+        super(props);
 
-export default modal;
+        this.setWrapperRef = this.setWrapperRef.bind(this);
+        this.handleClickOutside = this.handleClickOutside.bind(this);
+    }
+
+    componentDidMount() {
+        document.addEventListener('mousedown', this.handleClickOutside);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('mousedown', this.handleClickOutside);
+    }
+
+    setWrapperRef(node) {
+        this.wrapperRef = node;
+    }
+
+    handleClickOutside(event) {
+        if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+            this.props.onButtonClick();
+        }
+    }
+    render() {
+        return (
+            <div ref={this.setWrapperRef} className="MODAL">
+                <div className="MODAL__container">
+                    <h2>{this.props.header}</h2>
+                    <hr />
+                    <p>{this.props.content}</p>
+                    <Button class="btn-danger" clicked={this.props.onButtonClick}>
+                        Close
+                    </Button>
+                </div>
+            </div>
+        );
+    }
+}
